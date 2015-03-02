cba0860fe8cc79b6b12d3109898c6182c532b3e4
Kyle Goddard <kylegoddard@gmail.com>
2015-03-01 23:02:14 -0500

    umbrella header

---
+++ Pod/Classes/KGFloatingDrawer.h
@@ -0,0 +1,7 @@
+@import Foundation;
+
+//! Project version number for KGFloatingDrawer.
+FOUNDATION_EXPORT double KGFloatingDrawerVersionNumber;
+
+//! Project version string for KGFloatingDrawer.
+FOUNDATION_EXPORT const unsigned char KGFloatingDrawerVersionString[];
